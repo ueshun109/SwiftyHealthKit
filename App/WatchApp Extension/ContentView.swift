d8bc6cbd0484216974f5b98e8704c18e273d2e2f
yogita109 <ush109rock@gmail.com>
2021-05-28 12:16:20 +0900

    :+1: Update demo view

--- App/WatchApp Extension/ContentView.swift
+++ App/WatchApp Extension/ContentView.swift
@@ -1,16 +1,107 @@
+import Combine
+import HealthKit
 import SwiftUI
 import SwiftyHealthKit
 
 struct ContentView: View {
+  @ObservedObject var viewModel = ContentViewModel()
+  @State var isStart = false
+  private var cancellables: [AnyCancellable] = []
   var body: some View {
-    Text("Hello, World!")
-      .padding()
-  }
+    Button("Start") {
+      viewModel.start()
+      isStart.toggle()
+    }
+    .disabled(isStart)
 
+    Button("Add") {
+//      viewModel.add([HKMetadataKeyAverageMETs: HKQuantity(unit: HKUnit(from: "kg*hr"), doubleValue: 40)])
+      viewModel.add(["OriginalCalorie": 40])
+    }
+
+    Button("End") {
+      viewModel.end()
+      isStart.toggle()
+    }
+    .disabled(!isStart)
+    Text("\(viewModel.data)")
+  }
 }
 
-struct ContentView_Previews: PreviewProvider {
+final class ContentViewModel: ObservableObject {
+  private var cancellables: [AnyCancellable] = []
+  private let healthStore = HKHealthStore()
+  private let authorization: Authorization = .live
+  private let liveWorkout: LiveWorkout
+  private let workout: WorkoutFetcher = .live
+  @Published var data: Double = 0
+
+  init() {
+    self.liveWorkout = try! LiveWorkout(activityType: .swimming, healthStore: healthStore, locationType: .indoor, swimmingLocationType: .pool)
+    liveWorkout.data
+      .receive(on: DispatchQueue.main)
+      .sink { _ in
+      } receiveValue: { data in
+        self.data = data.heartRate
+      }
+      .store(in: &self.cancellables)
+  }
+
+  func start() {
+    let sendType: Set<HKSampleType> = [.workoutType()]
+    let readType: Set<HKObjectType> = [
+      HKObjectType.quantityType(forIdentifier: .heartRate)!,
+      HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!
+    ]
+    authorization.request(sendType, readType)
+      .mapError { $0 }
+      .sink { _ in
+      } receiveValue: { _ in
+        self.liveWorkout.start()
+      }
+      .store(in: &self.cancellables)
+  }
+
+  func end() {
+    liveWorkout.end()
+  }
+
+  func add(_ metadata: [String: Any]) {
+    liveWorkout.addEvent(metadata)
+      .sink { result in
+        switch result {
+        case let .failure(error):
+          print(error.message)
+        default: break
+        }
+      } receiveValue: { result in
+        print(result)
+      }
+      .store(in: &self.cancellables)
+  }
+}
+
+class Test {
+  private var cancellables: [AnyCancellable] = []
+  func test() {
+    let calendar = Calendar.current
+    let workout: WorkoutFetcher = .live
+    let now = Date()
+    let lastWeek = calendar.date(byAdding: .weekOfMonth, value: -1, to: now)!
+    workout.workouts(.swimming, now, lastWeek)
+      .sink { _ in
+
+      } receiveValue: { result in
+        print(result.count)
+      }
+      .store(in: &self.cancellables)
+  }
+}
+
+#if DEBUG
+struct ContenView_Preview: PreviewProvider {
   static var previews: some View {
     ContentView()
   }
 }
+#endif
--- App/iOS/ContentView.swift
+++ App/iOS/ContentView.swift
@@ -1,15 +1,68 @@
+import Combine
 import HealthKit
 import SwiftUI
 import SwiftyHealthKit
 
 struct ContentView: View {
+  @ObservedObject private var viewModel = ContentViewModel()
+  private var cancellables: [AnyCancellable] = []
   var body: some View {
-    Text("Hello world")
+    VStack {
+      Text("\(viewModel.calorie)")
+      Button("request") {
+        viewModel.request()
+      }
+    }
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
+final class ContentViewModel: ObservableObject {
+  private var cancellables: [AnyCancellable] = []
+  private let auth: Authorization = .live
+  private let workout: WorkoutFetcher = .live
+  @Published var calorie: Double = 0
+
+  func request() {
+    let readType: Set<HKObjectType> = [
+      HKObjectType.quantityType(forIdentifier: .heartRate)!,
+      HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+      .workoutType()
+    ]
+    auth.request(nil, readType)
+      .mapError { $0 }
+      .flatMap { [unowned self] _ -> Future<[HKWorkout], Error> in
+        let calendar = Calendar.current
+        let current = calendar.date(byAdding: .hour, value: 9, to: Date())!
+        let startToday = calendar.date(bySettingHour: 9, minute: 0, second: 0, of: Date())!
+        let yesterday = calendar.date(byAdding: .day, value: -1, to: startToday)!
+        return self.workout.workouts(.swimming, yesterday , startToday)
+      }
+      .mapError { $0 }
+      .flatMap { [unowned self] workouts -> Just<[DateComponents: Double?]> in
+        self.workout.metadataFromEvent(associatedWith: workouts, groupUnit: [.year, .month, .day], key: "OriginalCalorie")
+      }
+      .receive(on: DispatchQueue.main)
+      .sink { result in
+        switch result {
+        case let .failure(error):
+          print(error.localizedDescription)
+        default: break
+        }
+      } receiveValue: { [unowned self] metadata in
+        print(metadata)
+        guard let key = metadata.first?.key,
+              let value = metadata[key] as? Double
+        else { return }
+        self.calorie = value
+      }
+      .store(in: &self.cancellables)
+  }
+}
+
+#if DEBUG
+struct ContenView_Preview: PreviewProvider {
   static var previews: some View {
     ContentView()
   }
 }
+#endif
